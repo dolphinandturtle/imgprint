30c5130722ecca173e673a7cd1111d9298b54936
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2025-05-06 17:21:53 +0200

    Added pusheen and crude command arguments

--- term.py
+++ term.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     from load import Image
     from transform import downscale
     from encode import xterm256
-    fname = "warrior.ppm"
+    from sys import argv
+    fname = argv[1] if len(argv) > 3 else "warrior.ppm"
+    width, height = int(argv[2]), int(argv[3])
     with open(fname, 'rb') as file:
         img = Image.ppm_read(file)
-        WIDTH, HEIGHT = 80, 80
-        buf = [[0 for x in range(WIDTH)] for y in range(HEIGHT)]
+        buf = [[0x0 for x in range(width)] for y in range(height)]
         downscale(buf, img)
         buf = [list(map(xterm256, row)) for row in buf]
         print256(buf)
